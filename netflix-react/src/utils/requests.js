b25b27ab45ce91a2a696e09778b719ae8e67b4c5
dagimtsegaw <dagatse577@gmail.com>
2024-09-22 17:30:06 +0300

    bug-fix

--- netflix-react/src/utils/requests.js
+++ netflix-react/src/utils/requests.js
@@ -8,7 +8,7 @@ const requests = {
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&include_adult=false`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&include_adult=false`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&include_adult=false`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=36&include_adult=false`,
   fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`,
 };
 
