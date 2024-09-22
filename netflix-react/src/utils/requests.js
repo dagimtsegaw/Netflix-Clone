f9372692587d593fb61157d9a9a388f6b2154574
dagimtsegaw <dagatse577@gmail.com>
2024-09-22 17:07:16 +0300

    additional changes

--- netflix-react/src/utils/requests.js
+++ netflix-react/src/utils/requests.js
@@ -6,10 +6,10 @@ const requests = {
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&include_adult=false`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&include_adult=false`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&include_adult=false`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`,
 };
 
 export default requests;
